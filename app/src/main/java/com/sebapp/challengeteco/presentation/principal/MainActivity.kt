ec9d5f19ac3a48b6cedb28040ae831a8cc46967c
Sebastian <pratto.sebastian.ignacio@gmail.com>
2022-06-30 18:38:03 -0300

    added retrofit and sevices- second part

--- app/src/main/java/com/sebapp/challengeteco/presentation/principal/MainActivity.kt
+++ app/src/main/java/com/sebapp/challengeteco/presentation/principal/MainActivity.kt
@@ -2,6 +2,7 @@ package com.sebapp.challengeteco.presentation.principal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 import com.sebapp.challengeteco.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,4 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+
 }
