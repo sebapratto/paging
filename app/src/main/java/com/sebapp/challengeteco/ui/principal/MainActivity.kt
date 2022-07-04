861836128edfa4498ed7f3e84b33ca56cfc7fc0d
Sebastian <pratto.sebastian.ignacio@gmail.com>
2022-07-04 08:47:16 -0300

    pagin data services

---
+++ app/src/main/java/com/sebapp/challengeteco/ui/principal/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.sebapp.challengeteco.ui.principal
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.sebapp.challengeteco.R
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.flow.collectLatest
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerViewAdapter: RecyclerViewAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initRecyclerView()
+
+        initViewModel()
+
+        setWindowStyle()
+
+    }
+
+    private fun initRecyclerView() {
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            val decoration  = DividerItemDecoration(applicationContext, DividerItemDecoration.VERTICAL)
+            addItemDecoration(decoration)
+            recyclerViewAdapter = RecyclerViewAdapter()
+            adapter = recyclerViewAdapter
+
+        }
+    }
+
+    private fun initViewModel() {
+        val viewModel  = ViewModelProvider(this)[MainActivityViewModel::class.java]
+        lifecycleScope.launchWhenCreated {
+            viewModel.getListData().collectLatest {
+                recyclerViewAdapter.submitData(it)
+            }
+        }
+    }
+
+    /** Set window style */
+
+    private fun setWindowStyle() {
+
+        val window = window
+        window.statusBarColor = Color.TRANSPARENT
+        window.navigationBarColor = Color.TRANSPARENT
+
+        if(android.os.Build.VERSION.SDK_INT > 29){
+            window.setDecorFitsSystemWindows(false)
+        }else{
+            window.addFlags(WindowManager.LayoutParams.FIRST_SYSTEM_WINDOW)
+        }
+
+    }
+
+}
